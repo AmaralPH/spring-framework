387526dc083f2838bc06b2532bb13ce97e685a0c
Pedro Amaral <amaralph1995@gmail.com>
2022-04-01 22:25:43 -0300

    feat: exercicios sobre deploy no Jetty [7/7]

--- java-servlet-fundamentos/gerenciador/src/main/java/br/com/caelum/gerenciador/servlet/MonitoramentoFilter.java
+++ java-servlet-fundamentos/gerenciador/src/main/java/br/com/caelum/gerenciador/servlet/MonitoramentoFilter.java
@@ -1,6 +1,7 @@
 package br.com.caelum.gerenciador.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -14,7 +15,13 @@ import javax.servlet.http.HttpFilter;
  * Servlet Filter implementation class MonitoramentoFilter
  */
 @WebFilter("/entrada")
-public class MonitoramentoFilter extends HttpFilter implements Filter {
+public class MonitoramentoFilter implements Filter {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void destroy() {}
+	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		
 		long antes = System.currentTimeMillis();
